5fe51c30120f82532dce7f7a604b1be372a0b0a4
Manthan663 <manthanupadhyay11@gmail.com>
2026-04-30 16:30:58 +0530

    first commit

---
+++ src/js/Navbar.js
@@ -0,0 +1,115 @@
+import { menuItems } from "./constants/menu.js";
+function renderMenu(items, containerId) {
+  const container = document.getElementById(containerId);
+
+  container.innerHTML = items
+    .map((item) => {
+      const hasDropdown = item.items && item.items?.length > 0;
+      const hasMegaMenu = item.megaMenu?.columns?.length > 0;
+      const hassmallMenu = item.smallMenu?.columns?.length > 0;
+
+      return `
+      <li class="relative group ">
+
+        <a href="${item.link}" class="nav-link hover:text-stone-400 uppercase">
+          ${item.name}
+        </a>
+
+        ${
+          hasDropdown
+            ? `
+        <div class="fixed top-[145px] left-0  w-screen bg-white shadow-xl 
+        opacity-0 invisible group-hover:visible group-hover:opacity-100 
+        transition duration-300 z-50">
+
+          <div class=" max-w-7xl mx-auto py-8 flex  gap-10">
+
+            ${item.items
+              .map(
+                (sub) => `
+              <div class="flex flex-col items-center gap-2">
+              <div class="overflow-hidden">
+                <img src="${sub.img}" class="w-48 rounded-md object-cover transition-transform duration-300 ease-out hover:scale-110 cursor-pointer" />
+                
+                </div>
+                <span class="text-sm text-gray-500 mt-4">${sub.name || ""}</span>
+              </div>
+            `,
+              )
+              .join("")}
+
+          </div>
+        </div>
+        `
+            : ""
+        }
+${
+  hasMegaMenu
+    ? `
+<div class="fixed top-[145px] left-0 w-screen bg-white shadow-xl 
+opacity-0 invisible group-hover:visible group-hover:opacity-100 
+transition duration-300 z-50">
+
+  <div class="max-w-7xl mx-auto py-8 grid grid-cols-5 gap-10">
+
+    ${item.megaMenu.columns
+      .map(
+        (col) => `
+      <div>
+        <h4 class=" mb-4 text-lg">${col.title}</h4>
+
+        <ul class="space-y-6 border-r-2">
+          ${col.items
+            .map(
+              (text) => `
+            <li class="text-gray-500 cursor-pointer text-lg hover:text-stone-400">
+              ${text}
+            </li>
+          `,
+            )
+            .join("")}
+        </ul>
+      </div>
+    `,
+      )
+      .join("")}
+
+    <div class="overflow-hidden">
+      <img src="${item.megaMenu.banner}" class=" rounded-md hover:scale-110 transition-transform ease-out duration-300"/>
+    </div>
+
+  </div>
+</div>
+`
+    : ""
+}
+${hassmallMenu ?
+  ` <div class="fixed top-[145px] w-[200px]  bg-white shadow-xl 
+opacity-0 invisible group-hover:visible group-hover:opacity-100 
+transition duration-300 z-50">
+
+  <div class="max-w-xl mx-auto py-8 flex items-center justify-center gap-10">
+     ${item.smallMenu.columns.map((p)=>
+      `
+      <div>
+      <ul class="space-y-6">
+         ${p.items.map((texts)=>
+          `
+          <li class="text-gray-500 cursor-pointer text-lg hover:text-stone-400">
+          ${texts}
+          </li>
+          `
+         ).join("")}
+      </ul>
+      </div>
+      `).join("")}
+  </div>
+  </div>
+`:""}
+
+      </li>
+      `;
+    })
+    .join("");
+}
+renderMenu(menuItems, "navMenu");
---
+++ src/js/constants/menu.js
@@ -0,0 +1,94 @@
+export const menuItems = [
+  {
+    name: "Home",
+    link: "#",
+
+    items: [
+      { img: "../images/home6.webp", name: "06 Classic jewelry" },
+      { img: "../images/demo-menu-banner-1.webp", name: "01 Unique jewelry" },
+      { img: "../images/demo-menu-banner-2.webp", name: "02 Modern jewelry" },
+      {
+        img: "../images/demo-menu-banner-3.webp",
+        name: " 03 Traditional jewelry",
+      },
+      { img: "../images/demo-menu-banner-4.webp", name: "04 Luxury jewelry" },
+      { img: "../images/demo-menu-banner-5.webp", name: "05 Elegant jewelry" },
+    ],
+  },
+  {
+    name: "Shop",
+    link: "#",
+    megaMenu: {
+      columns: [
+        {
+          title: "Earnings",
+          items: [
+            "Blue heavy t-shirt",
+            "Brown sunglasses",
+            "Bucket shoes",
+            "Iamond rashmi ring",
+            "Floral gold bangle",
+            "Glitter diamond ring",
+          ],
+        },
+        {
+          title: "Necklace",
+          items: [
+            "Rose gold rings",
+            "Brown sunglasses",
+            "Bucket shoes",
+            "Color print sandal",
+            "Floral gold bangle",
+            "Simple Pearl earrings",
+          ],
+        },
+        {
+          title: "Rings",
+          items: [
+            "Blue heavy t-shirt",
+            "Brown sunglasses",
+            "Dark sunglasses",
+            "Half sleev tshirt",
+            "Formal shirt",
+            "Gold nose pin",
+          ],
+        },
+        {
+          title: "Bracelets",
+          items: [
+            "Blue heavy t-shirt",
+            "Brown sunglasses",
+            "Formal shoes",
+            "Formal shirt",
+            "Blue heavy t-shirt",
+            "Brown sunglasses",
+          ],
+        },
+      ],
+      banner: "../images/jewelry-4-menu-banner.webp",
+    },
+  },
+  { name: "Products", link: "#" },
+  { name: "Blog", link: "#" },
+  {
+    name: "Pages",
+    link: "#",
+
+    smallMenu: {
+      columns: [
+        {
+          items: [
+            "About us",
+            "Contact",
+            "Faq's",
+            "Privacy policy",
+            "Refund policy",
+            "Store location",
+            "Shipping & return",
+            "Terms & condition",
+          ],
+        },
+      ],
+    },
+  },
+];
